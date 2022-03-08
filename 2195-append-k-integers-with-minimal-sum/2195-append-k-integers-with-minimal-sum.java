81523df3ce21766a1391f03a77d458bdbadb889d
Anushka121 <61277156+Anushka121@users.noreply.github.com>
2022-03-08 13:10:42 +0530

    Time: 25 ms (88.56%), Space: 83.3 MB (33.42%) - LeetHub

--- 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.java
+++ 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.java
@@ -1,23 +1,36 @@
-
-        class Solution {
-  public long minimalKSum(int[] nums, int k) {
-    // sum of number(which means belong [1, k]) in origin array
-    long taken = 0;
-    Arrays.sort(nums);
-    // pivot number
-    int last = 0;
-    for (int num : nums) {
-      // detect duplicate number
-      if (num <= k && num != last) {
-        // found one in [1, k], so we increase k, as we need more to fill the taken number
-        k++;
-        taken += num;
-      }
-
-      // remember last number
-      last = num;
+class Solution {
+    public long minimalKSum(int[] nums, int k)
+    {
+        //for taking sum of all k sums(1--k)
+        long sumK=0;
+        //to check if duplicate present or not
+        int prevVal=0;
+        Arrays.sort(nums);
+        for(int val:nums)
+        {    
+            //if(the val which is in the range of (1--k) exist in array 
+            //means we have to take more k vals to create the (1--k) sum of array)
+            //hen will increment k
+            // we only require unique numbers hence will check duplicacy
+            if(k>=val&& prevVal!=val)
+            {
+                sumK+=val;
+                k++;
+            }
+            prevVal=val;
+        }
+        
+        // There is a maths formula to find sum of n series
+        // (n(n+1))/2
+        
+        //hench the sum of(1--k) will be like n series;
+        //(k(k+1))/2;// which will include the numbers that are present in the array
+        //hence to find the sum of all k numbers 
+        // need remove the value of number which is present in array;
+        // remove the sum of distnict element from array;
+        
+        return (long)(k+1)*k/2-sumK;
+        
     }
-
-    return (long) (k + 1) * k / 2 - taken;
-  }
 }
+    
