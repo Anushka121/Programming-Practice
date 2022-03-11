28a151e728f492f2a22af388d43a1e6070434849
Anushka121 <61277156+Anushka121@users.noreply.github.com>
2022-03-11 22:52:00 +0530

    Time: 1 ms (75.83%), Space: 43.2 MB (31.88%) - LeetHub

---
+++ 61-rotate-list/61-rotate-list.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+   public ListNode rotateRight(ListNode head, int k) {
+        ListNode x = head; // creating copy of head in x to iterate the list while keeping the head unchanged
+        if( x == null || k==0) return head; // if list is empty or if we don't have to rotate the list, return it
+        int n = 1;  // counting no. of nodes in the list 
+        while( x.next != null){
+            x = x.next;
+            n++;
+        }
+        int i = 1;
+        k = k%n;  // updaing k acc. to point 5
+        if(k==0 || n==1) return head; // list will not change in this case, so just return it
+        ListNode y = head;
+        while( i < n-k ){
+            y = y.next;
+            i++;
+        }
+        ListNode newHead = y.next; // Creating a new head point which points to (n-k)th node
+        y.next = null;   // pointing the (n- k-1)th node to null
+        x.next = head; // pointing the last pointer to first node.
+        
+        return newHead;
+    }
+}
