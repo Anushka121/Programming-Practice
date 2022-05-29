b5711149136be1873b448cf05ec2be1ab3158127
Anushka121 <61277156+Anushka121@users.noreply.github.com>
2022-05-29 20:40:01 +0530

    Time: 447 ms (8.71%), Space: 285.1 MB (8.33%) - LeetHub

---
+++ 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String arrangeWords(String text) 
+    {
+        String[] s=text.toLowerCase().split(" ");
+        Arrays.sort(s,(a,b)-> a.length()-b.length());
+        String str=s[0];
+        s[0]=str.substring(0, 1).toUpperCase() + str.substring(1);
+        String result=" ";
+        for(String string:s)
+        {
+            result=result+" "+string;
+        }
+        
+        return result.substring(2);
+    }
+}
